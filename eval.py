0571bd1e5e25e6e34b8334b3de35e8a396ca7e91
chanyu020816 <trafalgarlaw0816@gmail.com>
2025-03-21 18:21:56 -0400

    Update model

--- eval.py
+++ eval.py
@@ -1,45 +1,30 @@
 import argparse
 import os
-import torch
-import yaml
 
-from utils import eval_ood, eval_cifar100
-from utils import build_cifar100_dataloader
-from utils.utils import *
+import src
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--runs_folder', type=str, default='')
+    parser.add_argument(
+        '--model_type',
+        type=str,
+        default='classification',
+        choices=['classification', 'ovd_classification', 'hierarchical_classification'],
+    )
+    args = parser.parse_args()
 
-def main(config, runs_folder, ood_pred):
-    model = load_model(config["MODEL"], 100, config["PRETRAIN"])
-    model_path = os.path.join(runs_folder, "best_model.pth")
-    model.load_state_dict(torch.load(model_path))
-    model.to(config["DEVICE"])
-
-    log_folder = runs_folder
-    log_path = os.path.join(log_folder, "log.txt")
-    test_loader = build_cifar100_dataloader(config, config["DATA_PATH"], mode='test')
-    _, clean_accuracy = eval_cifar100.evaluate_cifar100_test(model, test_loader, config["DEVICE"])
-    print(f"Test Accuracy: {clean_accuracy}")
-
-    with open(log_path, "a") as log_file:
-        log_file.write(f"Test Accuracy: {clean_accuracy}\n")
-        log_file.flush()
-
-    if ood_pred:
-        all_predictions = eval_ood.evaluate_ood_test(model, config)
-        submission_df_ood = eval_ood.create_ood_df(all_predictions)
-        submission_df_ood.to_csv(os.path.join(runs_folder, "submission_ood.csv"), index=False)
-
-    return None
+    config_path = os.path.join(args.runs_folder, 'config.yaml')
+    if args.model_type == 'classification':
+        model = src.ClassificationModel(config_path, runs_folder=args.runs_folder)
+        model.eval()
+    elif args.model_type == 'ovd_classification':
+        pass
+    elif args.model_type == 'hierarchical_classification':
+        pass
+    else:
+        ValueError(f"Invalid model type: {args.model_type}")
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--config', type=str, default='configs/config.yaml')
-    parser.add_argument('--runs_folder', type=str, default='./trained_weights')
-    parser.add_argument('--ood_pred', action='store_true')
-    args = parser.parse_args()
-
-    with open(args.config, "r") as f:
-        print(args.config)
-        config = yaml.safe_load(f)
-    main(config, args.runs_folder, args.ood_pred)
+    main()
