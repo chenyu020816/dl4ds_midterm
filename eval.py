1894ab3153ada6298b6981af5265a2de8b95bb0e
chanyu020816 <trafalgarlaw0816@gmail.com>
2025-03-21 20:02:59 -0400

    Update model

--- eval.py
+++ eval.py
@@ -6,6 +6,7 @@ import src
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--runs_folder', type=str, default='')
+    parser.add_argument('--ood_pred', action='store_true')
     parser.add_argument(
         '--model_type',
         type=str,
@@ -17,14 +18,14 @@ def main():
     config_path = os.path.join(args.runs_folder, 'config.yaml')
     if args.model_type == 'classification':
         model = src.ClassificationModel(config_path, runs_folder=args.runs_folder)
-        model.eval()
+        model.eval(args.ood_pred)
     elif args.model_type == 'ovd_classification':
         model = src.OVDClassificationModel(
             config_path,
             "cifar100_text_embeddings.pt",
             runs_folder=args.runs_folder
         )
-        model.eval()
+        model.eval(args.ood_pred)
     elif args.model_type == 'hierarchical_classification':
         pass
     else:
